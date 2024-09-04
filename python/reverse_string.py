bb3b55f6898cf540e4f19f728071986fbc271411
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-09-04 14:19:32 -0500

    Delete python/reverse_string.py

--- python/reverse_string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(string):
-	reverse_string = ''
-	num = len(string) - 1
-	for i in string:
-		reverse_string += string[num]
-		num-=1
-	return reverse_string
-
-print reverse_string("Example String")
